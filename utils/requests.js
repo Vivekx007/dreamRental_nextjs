666eb9c0af51fee3c737c6142ed308b1fc70176e
vivek <vivek.kumar@cnetric.com>
2024-05-27 10:46:55 +0530

    modify apiDOmain

--- utils/requests.js
+++ utils/requests.js
@@ -1,4 +1,4 @@
-const apiDomain = process.env.NEXT_PUBLIC_API || null;
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // fetch all properties
 async function fetchProperties({ showFeatured = false } = {}) {
