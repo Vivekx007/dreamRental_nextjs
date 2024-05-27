18427f63c8d553fda463a648deb0d1d96414c533
vivek <vivek.kumar@cnetric.com>
2024-05-27 10:18:03 +0530

    modify page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import FeaturedProperties from '@/components/FeaturedProperties';
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/InfoBoxes';
-
+export const dynamic = 'force-dynamic';
 const HomePage = () => {
   return (
     <>
